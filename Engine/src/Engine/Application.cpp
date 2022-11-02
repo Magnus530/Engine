871e20ee5f5a2653625178504b0bde57fc54b38b
Magnus <249319@stud.inn.no>
2022-11-02 12:58:36 +0100

    Patch 4 Precompiled headers

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -1,17 +1,23 @@
+#include "epch.h"
 #include "Application.h"
+
+#include "Engine/Events/Event.h"
+#include "Engine/Events/ApplicationEvent.h"
+#include "Engine/Log.h"
 
 namespace Engine
 {
 	Application::Application()
-	{
-	}
+	{}
 
 	Application::~Application()
-	{
-	}
+	{}
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		E_TRACE(e);
+
 		while (true);
 	}
 }
--- premake5.lua
+++ premake5.lua
@@ -18,6 +18,9 @@ project "Engine"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	pchheader "epch.h"
+	pchsource "Engine/src/epch.cpp"
+
 	files
 	{
 		"%{prj.name}/src/**.h",
@@ -26,6 +29,7 @@ project "Engine"
 
 	includedirs 
 	{
+		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include"
 	}
 
