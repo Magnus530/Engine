a51b03231c8d19ff1f29382524ea2156eb6c834d
Magnus <249319@stud.inn.no>
2022-11-16 00:46:51 +0100

    Patch 31 Shader Asset Files

--- Engine/src/Platform/OpenGL/OpenGLShader.h
+++ Engine/src/Platform/OpenGL/OpenGLShader.h
@@ -3,11 +3,15 @@
 #include "Engine/Renderer/Shader.h"
 #include <glm/glm.hpp>
 
+// TODO: Remove 
+typedef unsigned int GLenum;
+
 namespace Engine
 {
 	class OpenGLShader : public Shader
 	{
 	public:
+		OpenGLShader(const std::string& filepath);
 		OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc);
 		virtual ~OpenGLShader();
 
@@ -22,6 +26,11 @@ namespace Engine
 
 		void UploadUniformMat3(const std::string& name, const glm::mat3& matrix);
 		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
+
+	private:
+		std::string ReadFile(const std::string& filepath);
+		std::unordered_map<GLenum, std::string> PreProcess(const std::string& source);
+		void Compile(const std::unordered_map<GLenum, std::string>& shaderSources);
 
 	private:
 		uint32_t m_RendererID;
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -135,43 +135,11 @@ public:
 
 		m_FlatColorShader.reset(Engine::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 
-		std::string textureShaderVertexSrc = R"(
-			#version 330 core
-
-			layout(location = 0) in vec3 a_Position;
-			layout(location = 1) in vec2 a_TexCoord;
-
-			uniform mat4 u_ProjectionView;
-			uniform mat4 u_Transform;
-
-			out vec2 v_TexCoord;
-
-			void main()
-			{
-				v_TexCoord = a_TexCoord;
-				gl_Position = u_ProjectionView * u_Transform * vec4(a_Position, 1.0);
-			}
-		)";
-
-		std::string textureShaderFragmentSrc = R"(
-			#version 330 core
-
-			layout(location = 0) out vec4 color;
-
-			in vec2 v_TexCoord;
-			uniform sampler2D u_Texture;
-
-
-			void main()
-			{
-				color = texture(u_Texture, v_TexCoord);
-			}
-		)";
-
-		m_TextureShader.reset(Engine::Shader::Create(textureShaderVertexSrc, textureShaderFragmentSrc));
+		m_TextureShader.reset(Engine::Shader::Create("assets/shaders/Texture.glsl"));
 
 		m_Texture = Engine::Texture2D::Create("assets/textures/checkerboard.png");
-		m_AlphaTexture = Engine::Texture2D::Create("assets/textures/wolf.png");
+
+		m_WolfLogoTexture = Engine::Texture2D::Create("assets/textures/wolf.png");
 
 		std::dynamic_pointer_cast<Engine::OpenGLShader>(m_TextureShader)->Bind();
 		std::dynamic_pointer_cast<Engine::OpenGLShader>(m_TextureShader)->UploadUniformInt("u_Texture", 0);
@@ -231,9 +199,9 @@ public:
 
 		m_Texture->Bind();
 		Engine::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
-		m_AlphaTexture->Bind();
-		Engine::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
+		m_WolfLogoTexture->Bind();
+		Engine::Renderer::Submit(m_TextureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
 		//Engine::Renderer::Submit(m_Shader, m_VertexArray);
 
@@ -258,7 +226,7 @@ private:
 	std::shared_ptr<Engine::Shader> m_FlatColorShader, m_TextureShader;
 	std::shared_ptr<Engine::VertexArray> m_SquareVA;
 
-	std::shared_ptr<Engine::Texture2D> m_Texture, m_AlphaTexture;
+	std::shared_ptr<Engine::Texture2D> m_Texture, m_WolfLogoTexture;
 
 	Engine::OrthographicCamera m_Camera;
 	glm::vec3 m_CameraPosition;
