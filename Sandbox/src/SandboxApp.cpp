a02809780ca0ba886438bc2350b7e4cabe258f3c
Magnus <249319@stud.inn.no>
2022-11-24 23:43:45 +0100

    Patch 48 Entity system is a part of Entity initilizer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -12,6 +12,7 @@ https://www.youtube.com/watch?v=JxIZbV_XjAs&list=PLlrATfBNZ98dC-V-N3m0Go4deliWHP
 #include "Engine/Objects/VisualObject.h"
 #include "Engine/AssetLoaders/ObjLoader.h"
 #include "Platform/OpenGL/OpenGLVertexArray.h"
+#include "Engine/Scene/EntityInitializer.h"
 
 class ExampleLayer : public Engine::Layer
 {
@@ -19,51 +20,6 @@ public:
 	ExampleLayer()
 		: Layer("Example"), m_OCameraController(1280.0f / 720.0f, true), m_PCameraController(50.0f, 1280.0f / 720.0f, 0.01f, 1000.0f)
 	{
-		/* ----- OBJ test start ----- */
-		std::vector<Engine::Vertex> vertices;
-		std::vector<uint32_t> indices;
-
-		Engine::ObjLoader::ReadFile("Pillar", vertices, indices);
-		m_ObjVA.reset(Engine::VertexArray::Create());
-		std::shared_ptr<Engine::VertexBuffer> ObjVB;
-		ObjVB.reset(Engine::VertexBuffer::Create(vertices.data(), vertices.size() * sizeof(Engine::Vertex))); // OpenGLVertexBuffer*	// for en vector av floats
-		ObjVB->SetLayout
-		({
-			{ Engine::ShaderDataType::Float3, "a_Position" },
-			{ Engine::ShaderDataType::Float3, "a_Normal" },
-			{ Engine::ShaderDataType::Float2, "a_TexCoord" }
-		});
-		m_ObjVA->AddVertexBuffer(ObjVB);
-		std::shared_ptr<Engine::IndexBuffer> ObjIB;
-		ObjIB.reset(Engine::IndexBuffer::Create(indices)); // OpenGLIndexBuffer*
-		m_ObjVA->SetIndexBuffer(ObjIB);
-		/* ----- OBJ test end ----- */
-
-		m_SquareVA.reset(Engine::VertexArray::Create()); // OpenGLVertexArray*
-		std::vector<float> squareVertices =
-		{
-		//    x      y	     z			uv
-			-0.5f, -0.5f,   0.0f,	0.0f, 0.0f,	//	Bottom	- Left 
-			 0.5f, -0.5f,   0.0f,	1.0f, 0.0f, //	Bottom	- Right
-			 0.5f,  0.5f,   0.0f,	1.0f, 1.0f, //	Top		- Right
-			-0.5f,  0.5f,   0.0f,	0.0f, 1.0f	//	Top		- Left
-		};;
-		
-		std::shared_ptr<Engine::VertexBuffer> SquareVB;
-		SquareVB.reset(Engine::VertexBuffer::Create(squareVertices.data(), squareVertices.size()*sizeof(float))); // OpenGLVertexBuffer*	// for en vector av floats
-		SquareVB->SetLayout
-		({
-			{ Engine::ShaderDataType::Float3, "a_Position" },
-			{ Engine::ShaderDataType::Float2, "a_TexCoord" }
-			});
-		m_SquareVA->AddVertexBuffer(SquareVB);
-		
-		std::vector<uint32_t> squareIndices = { 0, 1, 2, 2, 3, 0 };
-		
-		std::shared_ptr<Engine::IndexBuffer> SquareIB;
-		SquareIB.reset(Engine::IndexBuffer::Create(squareIndices)); // OpenGLIndexBuffer*
-		m_SquareVA->SetIndexBuffer(SquareIB);
-
 		auto flatShader = m_ShaderLibrary.Load("assets/shaders/Flat.glsl");
 		auto textureShader = m_ShaderLibrary.Load("assets/shaders/Texture.glsl");
 
@@ -78,13 +34,13 @@ public:
 
 		m_ActiveScene = std::make_shared<Engine::Scene>();
 
-		// Create entities here
-		auto square = m_ActiveScene->CreateEntity("Pillar");
-		square.AddComponent<Engine::RendererComponent>(glm::vec4{1.0f, 1.0f, 0.0f, 1.0f});
-		m_SquareEntity = square;
+		//Create entities here
 
-		auto obj = m_ActiveScene->CreateEntity("Obj");
-		m_ObjEntity = obj;
+		m_ObjEntity = Engine::EntityInitializer::GetInstance().EntityInit("Cube", m_ObjVA, m_ActiveScene);
+		m_ObjEntity.AddComponent<Engine::RendererComponent>(glm::vec4{ 0.0f, 1.0f, 0.0f, 1.0f });
+
+		m_SquareEntity = Engine::EntityInitializer::GetInstance().EntityInit(m_SquareVA, m_ActiveScene);
+		m_SquareEntity.AddComponent<Engine::RendererComponent>(glm::vec4{ 1.0f, 1.0f, 0.0f, 1.0f });
 	}
 
 	void OnUpdate(Engine::Timestep ts) override
@@ -100,25 +56,25 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
-		auto textureShader = m_ShaderLibrary.Get("Texture");
 		auto flatShader = m_ShaderLibrary.Get("Flat");
+		auto textureShader = m_ShaderLibrary.Get("Texture");
 
 		std::dynamic_pointer_cast<Engine::OpenGLShader>(flatShader)->UploadUniformFloat3("u_Color", 
-			m_SquareEntity.GetComponent<Engine::RendererComponent>().Color);
+			m_ObjEntity.GetComponent<Engine::RendererComponent>().Color);
 
 		//Engine::Renderer::Submit(flatShader, m_SquareVA, glm::mat4(1.0f));
 
-		m_Texture->Bind();
 		/* Test posisjonering */	
 		static float sin{};
 		sin += ts;
 		float testmovement = sinf(sin);
-		//Engine::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)) * glm::translate(glm::mat4(1.0f), glm::vec3(0, testmovement,0)));
+		m_Texture->Bind();
+		Engine::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)) * glm::translate(glm::mat4(1.0f), glm::vec3(0, testmovement,0)));
 
-		m_WolfLogoTexture->Bind();
+		//m_WolfLogoTexture->Bind();
 		//Engine::Renderer::Submit(textureShader, m_SquareVA, glm::scale(glm::mat4(1.0f), glm::vec3(1.5f)));
 
-		/* ----- OBJ TEST START ----- */
+		/* ----- OBJ START ----- */
 		auto objTComp = m_ObjEntity.GetComponent<Engine::TransformComponent>();
 		glm::vec3 currentPosition(objTComp.m_Position[3]);
 		glm::vec3 tempPos = objTComp.m_Transform[3];
@@ -127,7 +83,7 @@ public:
 		glm::mat4 tempM = objTComp.m_Scale * objTComp.m_Rotation * objTComp.m_Position;
 
 		Engine::Renderer::Submit(flatShader, m_ObjVA, objTComp.m_Transform);
-		/* ----- OBJ TEST END ----- */
+		/* ----- OBJ END ----- */
 
 		Engine::Renderer::EndScene();
 	}
@@ -135,13 +91,13 @@ public:
 	virtual void OnImGuiRender() override
 	{
 		ImGui::Begin("Settings");
-		if (m_SquareEntity)
+		if (m_ObjEntity)
 		{
 			ImGui::Separator();
-			auto& tag = m_SquareEntity.GetComponent<Engine::TagComponent>().Tag;
+			auto& tag = m_ObjEntity.GetComponent<Engine::TagComponent>().Tag;
 			ImGui::Text("%s", tag.c_str());
-			auto& squareColor = m_SquareEntity.GetComponent<Engine::RendererComponent>().Color;
-			ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
+			auto& squareColor = m_ObjEntity.GetComponent<Engine::RendererComponent>().Color;
+			ImGui::ColorEdit4("Obj Color", glm::value_ptr(squareColor));
 			ImGui::Separator();
 		}
 		ImGui::End();
@@ -170,6 +126,7 @@ private:
 	Engine::OrthographicCameraController m_OCameraController;
 
 	std::shared_ptr<Engine::VisualObject> m_Obj;
+	//std::shared_ptr<Engine::EntityInitializer> m_EInit;
 };
 
 class Sandbox : public Engine::Application
