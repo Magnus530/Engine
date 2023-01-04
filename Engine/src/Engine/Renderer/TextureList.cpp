2310464831d73e121f9c6e67fc6d1dda6b3172e1
Magnus <249319@stud.inn.no>
2023-01-04 19:31:20 +0100

    Patch 57 Added tree textures

--- Engine/src/Engine/Renderer/TextureList.cpp
+++ Engine/src/Engine/Renderer/TextureList.cpp
@@ -48,6 +48,8 @@ namespace Engine
 		Engine::Renderer::CreateTexture("Chess", "assets/textures/checkerboard.png", scene);
 		Engine::Renderer::CreateTexture("Wolf", "assets/textures/wolf.png", scene);
 		Engine::Renderer::CreateTexture("White", "assets/textures/white.png", scene);
+		Engine::Renderer::CreateTexture("Tree", "assets/textures/tree.png", scene);
+		Engine::Renderer::CreateTexture("Pine", "assets/textures/pine.png", scene);
 
 		Engine::Renderer::CreateSkybox("Sky", skyFaces, scene);
 		Engine::Renderer::CreateSkybox("Forest", forestFaces, scene);
